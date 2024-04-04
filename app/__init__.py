f323b21d53d3b54e63ace8b3a9f9c98f814587b3
Michael Leighton <michael138leighton@gmail.com>
2024-04-03 22:39:38 -0500

    to debug

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from .config import Config
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
+# app.secret_key = os.environ.get('FLASK_SECRET_KEY', 'default_secret_key')
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 from app import views
--- app/models.py
+++ app/models.py
@@ -1,1 +1,12 @@
 # Add any model classes for Flask-SQLAlchemy here
+from . import db
+
+class Movie(db.Model):
+    
+    __tablename__ = 'movies'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text)
+    poster = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
